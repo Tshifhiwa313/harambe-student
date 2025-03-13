906dbb3189c7d0b55e6727abfe7590de371337b1
Tshifhiwa313 <tanaymunyai@gmail.com>
2025-03-13 03:16:37 +0200

    Update header.php

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once 'include/config.php';
+require_once __DIR__ . '/../config/config.php';
 require_once 'include/functions.php';
 require_once 'include/auth.php';
 ?>
